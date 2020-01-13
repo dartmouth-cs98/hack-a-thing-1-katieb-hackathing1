312355354c7fe0c62e68019d81539e56d8005449
Katie Bernardez <katherine.o.bernardez.20@dartmouth.edu>
2020-01-13 10:18:29 -0500

    configuring game view

---
+++ Add 1/Add 1/GameViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Add 1
+//
+//  Created by Katie Bernardez on 1/13/20.
+//  Copyright © 2020 Katie Bernardez. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
